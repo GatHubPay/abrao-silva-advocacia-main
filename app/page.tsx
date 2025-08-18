fa3e10f59993506f9f0f4a94fc873642ae2b1172
Vitor Hugo <vitorhugo@github>
2025-08-18 14:15:17 -0300

    main

--- app/page.tsx
+++ app/page.tsx
@@ -441,6 +441,7 @@ export default function AbraoSilvaAdvocacia() {
                   })}
                 </div>
                 
+                
                 {/* Mobile Menu Items */}
                 {menuItems.map((item, index) => (
                   <button
